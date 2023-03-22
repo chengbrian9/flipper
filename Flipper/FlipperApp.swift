d319417fe78bce5ec463b0f3f585f359cf62af85
brian_cheng <chengbrian9@gmail.com>
2023-03-21 22:50:41 -0700

    new swift app

---
+++ Flipper/FlipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipperApp.swift
+//  Flipper
+//
+//  Created by Brian Cheng on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FlipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

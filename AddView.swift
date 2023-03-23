5bd9a7a507f70c13051d323915512fe4944d0f77
brian_cheng <chengbrian9@gmail.com>
2023-03-22 21:03:16 -0700

    add transaction menu

--- AddView.swift
+++ AddView.swift
@@ -7,9 +7,91 @@
 
 import SwiftUI
 
+//struct AddView: View {
+//
+//    @State private var selectedButton: TransactionType?
+//
+//    var body: some View {
+//        VStack(alignment: .leading, spacing: 20) {
+//            Text("New Transaction")
+//                .font(.title)
+//                .fontWeight(.bold)
+//                .padding(.horizontal)
+//            VStack(spacing: 20) {
+//                TransactionButton(type: .buy, selectedType: $selectedButton)
+//                TransactionButton(type: .sell, selectedType: $selectedButton)
+//                TransactionButton(type: .trade, selectedType: $selectedButton)
+//            }
+//            .padding(.horizontal)
+//        }
+//    }
+//}
 struct AddView: View {
+    
+    @State private var selectedType: TransactionType?
+    @State private var isNextPagePresented = false
+    
     var body: some View {
-        Text("Add")
-            .navigationTitle("Add")
+        NavigationView {
+            VStack {
+                Text("New Transaction")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    TransactionButton(type: .buy, selectedType: $selectedType)
+                    TransactionButton(type: .sell, selectedType: $selectedType)
+                    TransactionButton(type: .trade, selectedType: $selectedType)
+                }
+                .padding()
+                
+                Spacer()
+                
+                NavigationLink(destination: NextView(selectedType: selectedType), isActive: $isNextPagePresented) {
+                    EmptyView()
+                }
+                .hidden()
+                
+                Button(action: {
+                    self.isNextPagePresented = true
+                }) {
+                    Text("Next")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
+                .disabled(selectedType == nil)
+                .padding(.bottom, 20)
+                .padding(.trailing, 20)
+                .frame(alignment: .bottomTrailing)
+            }
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct NextView: View {
+    
+    let selectedType: TransactionType?
+    
+    var body: some View {
+        switch selectedType {
+        case .buy:
+            Text("Buy View")
+        case .sell:
+            Text("Sell View")
+        case .trade:
+            Text("Trade View")
+        case .none:
+            EmptyView()
+        }
     }
 }
---
+++ Flipper/TransactionButton.swift
@@ -0,0 +1,55 @@
+//
+//  TransactionButton.swift
+//  Flipper
+//
+//  Created by Brian Cheng on 3/21/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct TransactionButton: View {
+    
+    let type: TransactionType
+    @Binding var selectedType: TransactionType?
+    
+    var body: some View {
+        Button(action: {
+            self.selectedType = self.type
+        }) {
+            HStack {
+                Text(type.rawValue.capitalized)
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.trailing, 20)
+                    .foregroundColor(selectedType == type ? .green : .black)
+                
+                Image(systemName: selectedType == type ? "checkmark.circle.fill" : "circle")
+                    .foregroundColor(selectedType == type ? .green : .gray)
+                    .font(.system(size: 22))
+                    .frame(width: 20, height: 20)
+                    .padding(.trailing)
+                    .alignmentGuide(.trailing, computeValue: { d in
+                        d[HorizontalAlignment.trailing]
+                    })
+            }
+            .frame(maxWidth: UIScreen.main.bounds.width * 0.8)
+            .padding(20)
+            .background(Color.white)
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(selectedType == type ? .green : Color.gray.opacity(0.5))
+            )
+        }
+    }
+}
+
+
+enum TransactionType: String {
+    case buy
+    case sell
+    case trade
+}
+
